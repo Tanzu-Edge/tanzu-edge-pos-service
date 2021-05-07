d9b07a82d2e249f1cda1fe7a5db98fddedb4ea3f
Mike Wright <wrightmi@vmware.com>
2021-05-07 13:38:20 -0400

    turn off rabbitmq when using yugabyteDB

--- src/main/java/com/tanzu/posdataservice/messaging/RabbitMqConfig.java
+++ src/main/java/com/tanzu/posdataservice/messaging/RabbitMqConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("receiver")
+@Profile("receiver,rabbit")
 public class RabbitMqConfig {
 	public static final String topicExchangeName = "pos-exchange";
 
